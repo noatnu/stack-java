36fbc52408d420d17a6aa3930bacf961531e10f8
zch <noatnu@163.com>
2020-06-26 23:15:00 +0800

    1

---
+++ js-tree/src/main/java/com/gold/jstree/JsTreeApplication.java
@@ -0,0 +1,13 @@
+package com.gold.jstree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsTreeApplication.class, args);
+    }
+
+}
---
+++ js-tree/src/test/java/com/gold/jstree/JsTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gold.jstree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
