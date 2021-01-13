270636956f643a66d7fbfb63ae46acde55d91e1d
zch <noatnu@163.com>
2021-01-13 17:54:44 +0800

    添加后台模板

--- core-se/src/main/java/thread/me/VolatileTest.java
+++ core-se/src/main/java/thread/me/VolatileTest.java
@@ -1,7 +1,7 @@
 package thread.me;
 
 public class VolatileTest {
-    private boolean shouldExit = false;
+    private  boolean shouldExit = false;
 
     public void setShouldExit(boolean shouldExit) {
         System.out.println("[" + Thread.currentThread().getName() + "] setShouldExit to " + shouldExit);
@@ -10,21 +10,26 @@ public class VolatileTest {
 
     public void waitShouldExit() {
         System.out.println("[" + Thread.currentThread().getName() + "] waitShouldExit");
+        System.out.println(this.shouldExit);
         while (!this.shouldExit) {
-
+            System.out.println(this.shouldExit);
         }
+        System.out.println(this.shouldExit);
         System.out.println("[" + Thread.currentThread().getName() + "] waitShouldExit end");
     }
 
     public static void main(String[] args) {
         final VolatileTest t1 = new VolatileTest();
-        new Thread(() -> {
-            t1.waitShouldExit();
-        }, "T-1").start();
+        Runnable target = new Runnable() {
+            @Override
+            public void run() {
+                t1.waitShouldExit();
+            }
+        };
+        Thread thread = new Thread(target,"t-1" );
+        thread.start();
+
         t1.setShouldExit(false);
-        new Thread(() -> {
-            t1.waitShouldExit();
-        }, "T-2").start();
-        t1.setShouldExit(true);
+
     }
 }
